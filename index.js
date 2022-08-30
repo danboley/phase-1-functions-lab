32774e141b35c5a0a2b8dafff7ef85d711344081
Daniel Boley <danboley1@gmail.com>
2022-08-30 17:06:49 -0400

    commit tree 156832d93aabf16d08a4574a6b1f2d34cdd14801

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - hq;
+    }
+    else {
+        return hq - blocks;
+    };
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    }
+    else {
+        return (destination - start) * 264;
+    };
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return (distanceTravelledInFeet(start, destination) * 0);
+    }
+    if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return ((distanceTravelledInFeet(start, destination) - 400) * 0.02)
+    }
+    if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500)
+        return (25)
+    else {
+        return 'cannot travel that far';
+    }
+}
